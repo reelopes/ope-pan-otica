5a44d4414a7a61ba74c20587703fd2c88da1c825
eduardo.pereira2806@gmail.com <eduardo.pereira2806@gmail.com@33f5ab9e-7a79-613c-d616-a82c4c3248b0>
2013-03-20 23:47:48 +0000

    js nome de produto para util

---
+++ CI_Otica_Pan/public/js/util.js
@@ -0,0 +1,19 @@
+function mostraArmacao(){
+    var elem = document.getElementById("armacao");
+    elem.style.display = "visible";
+}
+
+function ocultaArmacao(){
+    var elem = document.getElementById("armacao");
+    elem.style.display = "none";
+}
+
+function mostraLente(){
+    var elem = document.getElementById("lente");
+    elem.style.display = "visible";
+}
+
+function ocultaLente(){
+    var elem = document.getElementById("lente");
+    elem.style.display = "none";
+}
