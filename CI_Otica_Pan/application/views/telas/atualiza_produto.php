9ca49da50dbd01b4431cf612578cdc8416d3773d
eduardo.pereira2806@gmail.com <eduardo.pereira2806@gmail.com@33f5ab9e-7a79-613c-d616-a82c4c3248b0>
2013-04-20 07:13:05 +0000

    LINDO!

--- CI_Otica_Pan/application/views/telas/atualiza_produto.php
+++ CI_Otica_Pan/application/views/telas/atualiza_produto.php
@@ -15,13 +15,63 @@ echo form_open("Produto/update/$id_produto");
 $todos_fornecedor = $this -> fornecedor_model -> getAll() -> result();
 $todas_grife = $this -> grife_model -> getAll() -> result();
 
+echo"<center><table>";
+// Campos da tabela Produto
+//echo"<center><table>";
+echo"<tr><td>";
+echo form_label('Referencia');
+echo"</td><td>";
+echo form_input(array('name'=>'referencia'), set_value('referencia', $query['produto']->referencia),'autofocus');
+echo"</td></tr>";
+echo"<tr><td>";
+echo form_label('Nome');
+echo"</td><td>";
+echo form_input(array('name'=>'nome'),
+        set_value('nome', $query['produto']->nome),'autofocus');
+echo"</td></tr>";
+echo"<tr><td>";
+echo form_label('Descricao');
+echo"</td><td>"; 
+echo form_input(array('name'=>'descricao'),
+        set_value('descricao', $query['produto']->descricao),'autofocus');
+echo"</td></tr>";
+echo"<tr><td>";
+echo form_label('Preco de custo');
+echo"</td><td>"; 
+echo form_input(array('name'=>'preco_custo'),
+        set_value('preco_custo', $query['produto']->preco_custo),'autofocus');
+echo"</td></tr>";
+echo"<tr><td>";
+echo form_label('Preco de venda');
+echo"</td><td>"; 
+echo form_input(array('name'=>'preco_venda'),
+        set_value('preco_venda', $query['produto']->preco_venda),'autofocus');
+echo"</td></tr>";
+echo"<tr><td>";
+echo form_label('Quantidade');
+echo"</td><td>"; 
+echo form_input(array('name'=>'quantidade'),
+        set_value('quantidade', $query['produto']->quantidade),'autofocus');
+echo"</td></tr>";
+echo"<tr><td>";
+echo form_label('Status');
+echo"</td><td>"; 
+echo form_input(array('name'=>'status'),
+        set_value('status', $query['produto']->status),'autofocus');
+echo"</td></tr>";
+echo"<tr><td>";
+echo form_label('Validade');
+echo"</td><td>"; 
+echo form_input(array('name'=>'validade'),
+        set_value('validade', $query['produto']->validade),'autofocus');
+echo"</td></tr>";
+
 // Oculta campos e mostra campos de acordo com a escolha
 if($query['produto']->categoria == 1) {
 // div armacao, carrega campos de armacao
 echo'<div id="armacao">';
 echo"<p>";
-echo"<table>";
-echo"<tr><td>";
+echo"<tr><td>"; 
 echo form_label('Largura da lente');
 echo"</td><td>"; 
 echo form_input(array('name'=>'largura_lente'), set_value('largura_lente', $query['armacao']->largura_lente),'autofocus');
@@ -73,66 +123,18 @@ if ($todos_fornecedor != NULL) {
 echo'</select>';
 echo"</td></tr>";
 echo"<tr><td>";
-echo"</table>"; 
+//echo"</table>"; 
 echo"</div>";
 } else {
     
 }
 
-// Campos da tabela Produto
-echo"<center><table>";
-echo"<tr><td>";
-echo form_label('Referencia');
-echo"</td><td>";
-echo form_input(array('name'=>'referencia'), set_value('referencia', $query['produto']->referencia),'autofocus');
-echo"</td></tr>";
-echo"<tr><td>";
-echo form_label('Nome');
-echo"</td><td>";
-echo form_input(array('name'=>'nome'),
-        set_value('nome', $query['produto']->nome),'autofocus');
-echo"</td></tr>";
-echo"<tr><td>";
-echo form_label('Descricao');
-echo"</td><td>"; 
-echo form_input(array('name'=>'descricao'),
-        set_value('descricao', $query['produto']->descricao),'autofocus');
-echo"</td></tr>";
-echo"<tr><td>";
-echo form_label('Preco de custo');
-echo"</td><td>"; 
-echo form_input(array('name'=>'preco_custo'),
-        set_value('preco_custo', $query['produto']->preco_custo),'autofocus');
-echo"</td></tr>";
-echo"<tr><td>";
-echo form_label('Preco de venda');
-echo"</td><td>"; 
-echo form_input(array('name'=>'preco_venda'),
-        set_value('preco_venda', $query['produto']->preco_venda),'autofocus');
-echo"</td></tr>";
-echo"<tr><td>";
-echo form_label('Quantidade');
-echo"</td><td>"; 
-echo form_input(array('name'=>'quantidade'),
-        set_value('quantidade', $query['produto']->quantidade),'autofocus');
-echo"</td></tr>";
-echo"<tr><td>";
-echo form_label('Status');
-echo"</td><td>"; 
-echo form_input(array('name'=>'status'),
-        set_value('status', $query['produto']->status),'autofocus');
-echo"</td></tr>";
-echo"<tr><td>";
-echo form_label('Validade');
-echo"</td><td>"; 
-echo form_input(array('name'=>'validade'),
-        set_value('validade', $query['produto']->validade),'autofocus');
-echo"</td></tr>";
 
 echo"</tr><td>"; 
 echo"<td>"; 
 echo form_submit('', 'Alterar');
 echo"</td><tr>"; 
+
 echo"</table>";
 
 echo form_hidden('id_produto',$id_produto);
