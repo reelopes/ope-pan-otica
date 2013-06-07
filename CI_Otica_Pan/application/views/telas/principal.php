96d42909c72d84af6c92df2f285507dcc463578c
eduardo.pereira2806@gmail.com <eduardo.pereira2806@gmail.com@33f5ab9e-7a79-613c-d616-a82c4c3248b0>
2013-06-07 07:58:00 +0000

    tela principal reorganizada

--- CI_Otica_Pan/application/views/telas/principal.php
+++ CI_Otica_Pan/application/views/telas/principal.php
@@ -4,6 +4,37 @@
 <table width="80%" border="0" class="iconesPrincipal">
     <tr>
         <td align="center">
+            <a href="<? echo base_url('fluxoFinanceiro'); ?>" >
+                <img src="public/img/report.png" width="74px" heigth="74px"/>
+                <br />
+                Relatório de Fluxo Finaceiro
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('cheque'); ?>" >
+                <img src="public/img/cheque_2.png" width="74px" heigth="74px"/>
+                <br />
+                Gerenciar Cheques
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('agendamento'); ?>">
+                <img src="public/img/agendamento.png" width="72px" heigth="72px"/>
+                <br />
+                Agendamento
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('venda'); ?>">
+                <img src="public/img/venda.png" width="74px" heigth="74px"/>
+                <br />
+                Venda de Produtos/Serviço
+            </a>
+        </td>
+    </tr>
+    <tr><td><br></td></tr>
+    <tr>
+        <td align="center">
             <a href="<? echo base_url('cliente'); ?>" >
                 <img src="public/img/cadastro_cliente.png"/>
                 <br />
@@ -17,8 +48,6 @@
                 Pesquisa de Clientes  
             </a>
         </td>
-
-
         <td align="center">
             <a href="<? echo base_url('fornecedor/adiciona'); ?>">
                 <img src="public/img/cadastro_fornecedor.png"/>
@@ -36,9 +65,15 @@
             </a>
         </td>
     </tr>
-    <tr><td><br></td></tr>
     <tr>
-    <td align="center">
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td align="center">
             <a href="<? echo base_url('produto'); ?>">
                 <img src="public/img/cadastro_produto.png"/>
                 <br />
@@ -50,20 +85,6 @@
                 <img src="public/img/pesquisa_produto.png"/>
                 <br />
                 Pesquisa de Produtos
-            </a>
-        </td>
-         <td align="center">
-            <a href="<? echo base_url('agendamento'); ?>">
-                <img src="public/img/agendamento.png" width="72px" heigth="72px"/>
-                <br />
-                Agendamento
-            </a>
-        </td>
-        <td align="center">
-            <a href="<? echo base_url('venda'); ?>">
-                <img src="public/img/venda.png" width="74px" heigth="74px"/>
-                <br />
-                Venda de Produtos/Serviço
             </a>
         </td>
     </tr>
