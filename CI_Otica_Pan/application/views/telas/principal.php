c0d4b4174189aba65ad88248802fa02d75f327bb
eduardo.pereira2806@gmail.com <eduardo.pereira2806@gmail.com@33f5ab9e-7a79-613c-d616-a82c4c3248b0>
2013-06-07 09:13:57 +0000

    banco - tirei o nivel caixa e usuario caixa

--- CI_Otica_Pan/application/views/telas/principal.php
+++ CI_Otica_Pan/application/views/telas/principal.php
@@ -1,5 +1,8 @@
 <br>
 <br>
+<?
+    if ($this->session->userdata('id_nivel') == "1") {
+?>
 
 <table width="80%" border="0" class="iconesPrincipal">
     <tr>
@@ -75,14 +78,14 @@
         <td>&nbsp;</td>
         <td align="center">
             <a href="<? echo base_url('produto'); ?>">
-                <img src="public/img/produto.png" width="74px" heigth="74px"/>
+                <img src="public/img/adiciona_produto_1.png" width="74px" heigth="74px"/>
                 <br />
                 Cadastro de Produtos
             </a>
         </td>
         <td align="center">
             <a href="<? echo base_url('produto/lista'); ?>" >
-                <img src="public/img/lista_produto.png" width="74px" heigth="74px"/>
+                <img src="public/img/lista_produto_1.png" width="74px" heigth="74px"/>
                 <br />
                 Pesquisa de Produtos
             </a>
@@ -96,5 +99,181 @@
     </tr>
 </table>
 
+<?
+    } else if ($this->session->userdata('id_nivel') == "2") {
+?>
+
+<table width="80%" border="0" class="iconesPrincipal">
+    <tr>
+        <td align="center">
+            <a href="<? echo base_url('cheque'); ?>" >
+                <img src="public/img/cheque_2.png" width="74px" heigth="74px"/>
+                <br />
+                Gerenciar Cheques
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('agendamento'); ?>">
+                <img src="public/img/agendamento.png" width="72px" heigth="72px"/>
+                <br />
+                Agendamento
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('venda'); ?>">
+                <img src="public/img/venda.png" width="74px" heigth="74px"/>
+                <br />
+                Venda de Produtos/Serviço
+            </a>
+        </td>
+    </tr>
+    <tr><td><br></td></tr>
+    <tr>
+        <td align="center">
+            <a href="<? echo base_url('cliente'); ?>" >
+                <img src="public/img/cadastro_cliente.png"/>
+                <br />
+                Cadastro de Clientes
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('cliente/listarClientes'); ?>">
+                <img src="public/img/pesquisa_cliente.png"/>
+                <br />
+                Pesquisa de Clientes  
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td align="center">
+            <a href="<? echo base_url('produto'); ?>">
+                <img src="public/img/adiciona_produto_1.png" width="74px" heigth="74px"/>
+                <br />
+                Cadastro de Produtos
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('produto/lista'); ?>" >
+                <img src="public/img/lista_produto_1.png" width="74px" heigth="74px"/>
+                <br />
+                Pesquisa de Produtos
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+</table>
+
+<?
+    } else if ($this->session->userdata('id_nivel') == "3") {
+?>
+
+<table width="80%" border="0" class="iconesPrincipal">
+    <tr>
+        <td align="center">
+            <a href="<? echo base_url('fluxoFinanceiro'); ?>" >
+                <img src="public/img/report.png" width="74px" heigth="74px"/>
+                <br />
+                Relatório de Fluxo Finaceiro
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('cheque'); ?>" >
+                <img src="public/img/cheque_2.png" width="74px" heigth="74px"/>
+                <br />
+                Gerenciar Cheques
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('agendamento'); ?>">
+                <img src="public/img/agendamento.png" width="72px" heigth="72px"/>
+                <br />
+                Agendamento
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('venda'); ?>">
+                <img src="public/img/venda.png" width="74px" heigth="74px"/>
+                <br />
+                Venda de Produtos/Serviço
+            </a>
+        </td>
+    </tr>
+    <tr><td><br></td></tr>
+    <tr>
+        <td align="center">
+            <a href="<? echo base_url('cliente'); ?>" >
+                <img src="public/img/cadastro_cliente.png"/>
+                <br />
+                Cadastro de Clientes
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('cliente/listarClientes'); ?>">
+                <img src="public/img/pesquisa_cliente.png"/>
+                <br />
+                Pesquisa de Clientes  
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('fornecedor/adiciona'); ?>">
+                <img src="public/img/cadastro_fornecedor.png"/>
+                <br />
+                Cadastro de Fornecedores    
+            </a>
+        </td>
 
 
+        <td align="center">
+            <a href="<? echo base_url('fornecedor/lista'); ?>">
+                <img src="public/img/pesquisa_fornecedor.png"/>
+                <br />
+                Pesquisa de Fornecedores    
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td align="center">
+            <a href="<? echo base_url('produto'); ?>">
+                <img src="public/img/adiciona_produto_1.png" width="74px" heigth="74px"/>
+                <br />
+                Cadastro de Produtos
+            </a>
+        </td>
+        <td align="center">
+            <a href="<? echo base_url('produto/lista'); ?>" >
+                <img src="public/img/lista_produto_1.png" width="74px" heigth="74px"/>
+                <br />
+                Pesquisa de Produtos
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+</table>
+
+<?
+    }
+?>
